462ae1734e09a85f2c10723808addfd953a4cd01
Raviteja <bompallyraviteja123@gmail.com>
2025-03-10 21:09:45 +0530

    10-March-2025 Java Comments program

---
+++ src/ex_01_Java_Basic/Comment001.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Comment001 {
+    public static void main(String[] args) {
+        System.out.println("Iam raviteja");
+        System.out.print("Iam Manual Test engineer");// it will print in single line
+        System.out.print("with 4+ years of exp");// we have taken as print so it will print in single line
+    }
+}
