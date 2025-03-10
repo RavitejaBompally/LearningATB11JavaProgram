de103eb8eea9649dcf3bdddb1689f0f0bc0e261f
Raviteja <bompallyraviteja123@gmail.com>
2025-03-10 21:23:17 +0530

    10-March-2025 Java Multiple Comments program

---
+++ src/Comment002.java
@@ -0,0 +1,18 @@
+public class Comment002 {
+    public static void main(String[] args) {
+        System.out.println("comment.java");
+        /*/we can comment multiple lines
+
+         */
+        int a=10;
+        int b=5;
+        System.out.println(a+b); //15
+        System.out.println(a-b); //5
+        System.out.println(a*b); //50
+        System.out.println(a/b); //2
+        System.out.println(a++); //10
+
+
+
+    }
+}
