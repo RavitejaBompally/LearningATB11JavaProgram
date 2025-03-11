1aeb7db7426568012f501a7f4e96582a3c500ef8
Raviteja <bompallyraviteja123@gmail.com>
2025-03-11 21:55:13 +0530

    11-March-2025 Printed the table of 9 by using the printf command

---
+++ src/Lab002.java
@@ -0,0 +1,15 @@
+public class Lab002 {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.printf("%d*1=%d%n" ,num,(num));
+        System.out.printf("%d*2=%d%n" ,num,(num));
+        System.out.printf("%d*3=%d%n" ,num,(num));
+        System.out.printf("%d*4=%d%n" ,num,(num));
+        System.out.printf("%d*5=%d%n" ,num,(num));
+        System.out.printf("%d*6=%d%n" ,num,(num));
+        System.out.printf("%d*7=%d%n" ,num,(num));
+        System.out.printf("%d*8=%d%n" ,num,(num));
+        System.out.printf("%d*9=%d%n" ,num,(num));
+        System.out.printf("%d*10=%d%n" ,num,(num));
+    }
+}
